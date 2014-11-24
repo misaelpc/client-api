b21f98e2e90655e1b6f5954510c84ce8123f51c3
fernando luna <fernando.luna@diverza.com>
2014-11-23 19:02:52 -0600

    se agrega funcionalidad para servicio de datos fiscales

--- web/controllers/client_controller.ex
+++ web/controllers/client_controller.ex
@@ -10,4 +10,25 @@ defmodule Router.ClientController do
     json conn, JSON.encode!(database)
   end
 
+  def datos_fiscales(conn, _params) do
+    
+    :odbc.start()
+    {:ok,_ref} = :odbc.connect('DSN=Hetzner;UID=sa;PWD=RakuRaku1',[])
+    query = 'SELECT u.idinternal AS domicilioId, u.calle, u.codigoPostal, u.colonia, u.estado, u.localidad, u.municipio, u.noExterior, u.noInterior, u.pais, u.referencia, e.idInternal AS empresaId, s.nombre, s.alias, s.idinternal AS sucursalId FROM empresa e INNER JOIN ubicacion u ON u.idinternal = e.ubicacion INNER JOIN sucursal s ON s.empresa_id = e.idinternal WHERE e.rfc = ?'
+    
+    query_parameter = [{{:sql_varchar, 13}, [to_char_list(_params["rfc"])]}]
+    datos_fiscales = :odbc.param_query(_ref, query, query_parameter)
+
+    :odbc.stop()
+    
+    json conn, JSON.encode!(datos_fiscales)
+  end
+
+  def status(conn, _params) do
+    :odbc.start
+    database = :odbc.connect('DSN=Hetzner;UID=sa;PWD=RakuRaku1', [])
+
+    json conn, JSON.encode!(database)
+  end
+
 end
--- web/router.ex
+++ web/router.ex
@@ -8,10 +8,18 @@ defmodule Router.Router do
     get "/", Router.PageController, :index, as: :pages
   end
 
-
-
   #Other scopes may use custom stacks.
   scope "api/v1/clients/:rfc" do
     get "/", Router.ClientController, :client
+  end
+
+  #obtiene los datos fiscales de un rfc
+  scope "api/v1/clients/datosFiscales/:rfc" do
+    get "/", Router.ClientController, :datos_fiscales
+  end
+
+  #obtiene el estatus de un rfc
+  scope "api/v1/clients/status/:rfc" do
+    get "/", Router.ClientController, :status
   end
 end
