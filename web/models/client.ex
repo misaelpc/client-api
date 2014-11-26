261f1fb05507e2483536fc3c915242d983bfaad9
misaelpc <misaelpcgm@gmail.com>
2014-11-26 15:36:20 -0600

    Revome comments on client model

--- web/models/client.ex
+++ web/models/client.ex
@@ -19,17 +19,4 @@ defmodule Client do
     [street: client.street, zip_code: client.zip_code]
 	end
 
-	# def client_permissions(rfc) do
-	# 	:odbc.start()
- #    {:ok,_ref} = :odbc.connect('DSN=Hetzner;UID=sa;PWD=RakuRaku1',[])
- #    query = 'SELECT cp.idInternal AS configuracionPermisosId, cp.generaCfdPortal, cp.generaCfdPost, cp.generaCfdWS, cp.cancelaCfdPortal, cp.cancelaCfdPost, cp.cancelaCfdWS, cp.transformaCfdPost, cp.reporteMensualSatPortal, cp.reporteMensualSatWS, cp.envioLigaDescarga, cp.trackingEmail, cp.envioCfdSftp, cp.envioCfdCorreo, cp.sftp, cp.email, cp.ws, cp.fileUpload, cp.wsBase64, cp.generaTimbreWS, cp.generaCfdiWS, cp.codigoQrWS, cp.envioXMLPdf, cp.nominaEmisionWS, cp.nominaTimbreWS FROM CONFIGURACION_PERMISOS cp INNER JOIN EMPRESA e on cp.idInternal = e.configuracionPermisos WHERE e.rfc = ?'
-
- #    query_parameter = [{{:sql_varchar, 13}, [rfc]}]
- #    status = :odbc.param_query(_ref, query, query_parameter)
-
- #    IO.puts status
- #    :odbc.stop()
- #    status
-	# end
-
 end
